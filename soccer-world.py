58586a614ba85d449c472bb0062852bd7ff32a14
ankitrustagi3 <arustagi3@gmail.com>
2023-06-11 20:13:10 -0700

    Retrieve data using BeautifulSoup and send to csv file

--- soccer-world.py
+++ soccer-world.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from string import ascii_uppercase as alphabet 
 import pickle
+from bs4 import BeautifulSoup
+import requests
 
 all_tables = pd.read_html('https://en.wikipedia.org/wiki/2022_FIFA_World_Cup')
 
@@ -19,3 +21,28 @@ for letter, i in zip(alphabet, range(9, 65, 7)):
 with open('dict_table', 'wb') as output:
     pickle.dump(dict_table, output)
 
+def get_matches(year):
+    web = f'https://en.wikipedia.org/wiki/{year}_FIFA_World_Cup'
+    response = requests.get(web)
+    content = response.text
+    soup = BeautifulSoup(content, 'lxml')
+
+    matches = soup.find_all('div', class_= 'footballbox')
+
+    home = []
+    away = []
+    score = []
+
+    for match in matches:
+        home.append(match.find('th', class_ = 'fhome').get_text())
+        score.append(match.find('th', class_ = 'fscore').get_text())
+        away.append(match.find('th', class_ ='faway').get_text())
+
+    dict_football = {'home': home, 'away': away, 'score': score}
+    df_football = pd.DataFrame(dict_football)
+    df_football['year'] = year
+
+    return df_football
+
+matchSummary = get_matches(2022)
+matchSummary.to_csv('fifa_worldcup_2022.csv', index=False)
